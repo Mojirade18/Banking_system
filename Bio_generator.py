e51396c72ec7bc794146ec38a87812d0aceaf91f
Mojirade18 <mojiradeoluranti@gmail.com>
2025-08-12 22:53:54 +0100

    A new project

--- Bio_generator.py
+++ Bio_generator.py
@@ -1,8 +1,19 @@
 print('Welcome to the Mini Bio Generator!')
 name = input("Enter your name: ").lower().strip()
-age = ('Enter your age: ')
-favourite_number = ('Enter your favourite number: ')
-hobby = ('Write a short sentence about your hobby: ')
+age = input('Enter your age: ')
+favourite_number = input('Enter your favourite number: ')
+hobby = input('Write a short sentence about your hobby: ')
 print(f"Lower case name: {name}")
 print(f"Upper case name: {name.upper()}")
 print(f"First three letters of your name: {name[:3]}")
+space_cup =[]
+non_space_cup = []
+new_hobby = ''
+for i, space in enumerate(hobby):
+    if space == " ":
+        space_cup.append(space)
+        new_hobby += "❤"
+    else:
+        non_space_cup.append(space)
+        new_hobby += space
+print(f"Modified hobby with emoji: {new_hobby}")
