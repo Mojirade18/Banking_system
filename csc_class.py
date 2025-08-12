b7aacfa02f2cd6291914e6c02eabf9ccbf2ebf8d
Mojirade18 <mojiradeoluranti@gmail.com>
2025-08-12 15:37:43 +0100

    A new project

--- csc_class.py
+++ csc_class.py
@@ -1,6 +1,8 @@
 paragraph = input("insert a paragraph of your choice: ").lower().strip()
+count = 0
 if 'p' in paragraph:
-    print("There is p your sentence")
+    count += 1
+    print(f"There are {count} p's in your sentence")
     
 else:
     print('there is no p')
