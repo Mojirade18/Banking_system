5c560ae550f58d1b06f660faf3026c6a23b091b9
Mojirade18 <mojiradeoluranti@gmail.com>
2025-08-13 16:15:01 +0100

    A new project

--- calculator.py
+++ calculator.py
@@ -1,1 +1,10 @@
 # This is a calculator
+name = input("Enter your name: ")
+age = ('Enter your age:')
+favorite_number = ("Enter your favorite number:") 
+mult = favorite_number * 10
+print(f"Hello {name}! you are {age} years old.")
+print(f"Your favorite number multiplied by 10 is {mult}.")
+if favorite_number > 50:
+    print("True, your favorite number is graeter than 50.")
+    
